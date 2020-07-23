3141829712e66f3846f149ef6daa5c932141cf1d
Nonz <ondrej.smarda@student.mgvsetin,cz>
2020-07-23 23:09:16 +0200

    Prototyp v0.1
    Přidal základní pohyb hráče
    Přidal základní pohyb nepřítele
    Přidal schovávaní za kryty

---
+++ Projekt Silons/Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Variables
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float distance;
+    private bool ignoreCollider;
+    public Transform wayPoint1, wayPoint2;
+    public Player player;
+    private Transform wayPointTarget;
+    private Transform target;
+
+
+    private void Awake()
+    {
+        wayPointTarget = wayPoint1;
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Vector3.Distance(transform.position, target.position) > distance)
+        {
+            Physics2D.IgnoreCollision(GetComponent<CapsuleCollider2D>(), player.GetComponent<CapsuleCollider2D>(), ignoreCollider = true);
+
+            if(Vector3.Distance(transform.position, wayPoint1.position) < 0.75f)
+            {
+                wayPointTarget = wayPoint2;
+            }
+
+            if(Vector3.Distance(transform.position, wayPoint2.position) < 0.75f)
+            {
+                wayPointTarget = wayPoint1;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, wayPointTarget.position, moveSpeed * Time.deltaTime);
+        }
+        else if(player.behindCover == false)
+        {
+            Physics2D.IgnoreCollision(GetComponent<CapsuleCollider2D>(), player.GetComponent<CapsuleCollider2D>(), ignoreCollider = false);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            Debug.Log("Chasing Player");
+        }
+        
+    }
+}
---
+++ Projekt Silons/Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+
+    public Rigidbody2D rb;
+
+    [SerializeField] float speed;
+    [SerializeField] float runningSpeed;
+    [SerializeField] float crouchSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float coverDistance;
+    private Vector3 coverCheckDir;
+    private float horInput;
+    private bool crouched = false;
+    public bool behindCover;
+
+    public Transform groundCheck;
+    private bool grounded;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
+
+
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    private void Update()
+    {
+        //Assigning player direction
+        horInput = Input.GetAxis("Horizontal");
+
+        //Checking if player is on the ground
+        grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+    }
+
+    void FixedUpdate()
+    {
+        //Adding horizontal velocity for walking
+        if (!behindCover)
+        {
+            rb.velocity = new Vector2(horInput * speed * Time.deltaTime, rb.velocity.y);
+        }
+
+        //Adding velocity for running 
+        if (!behindCover && Input.GetKey(KeyCode.LeftShift))
+        {
+            if(speed < maxSpeed)
+            {
+                rb.velocity += new Vector2(horInput * runningSpeed * Time.deltaTime, 0);
+            }
+        }
+
+        //Adding velocty for crouching and crouch toggle
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (!crouched)
+            {
+                speed = speed / 2;
+                crouched = true;
+                anim.SetBool("Crouched", true);
+            }
+            else
+            {
+                speed = speed * 2;
+                crouched = false;
+                anim.SetBool("Crouched", false);
+            }
+        }
+
+        //Adding vertical velocity
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = Vector2.up * jumpHeight * Time.deltaTime;
+        }
+
+        //Going out of cover
+        if(behindCover && Input.GetKeyDown(KeyCode.S))
+        {
+            behindCover = !behindCover;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Cover"))
+        {
+            if (!behindCover && Input.GetKeyDown(KeyCode.W))
+            {
+                behindCover = !behindCover;
+                rb.velocity = Vector2.zero;
+                transform.position = new Vector3(collider.transform.position.x, collider.transform.position.y, 6);
+            }
+        }
+    }
+
+}
